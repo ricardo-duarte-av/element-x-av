337fb65c89ee1914f88fba44d0409862b367bcaf
ricardo-duarte-av <ricardo.duarte@aguiarvieira.pt>
2025-05-05 22:33:42 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
 
     defaultConfig {
         //applicationId = BuildTimeConfig.APPLICATION_ID
-        applicationId = "net.daedric.element"
+        applicationId = "pt.aguiarvieira.element"
         targetSdk = Versions.TARGET_SDK
         versionCode = Versions.VERSION_CODE
         versionName = Versions.VERSION_NAME
@@ -104,7 +104,7 @@ android {
     }
 
     //val baseAppName = BuildTimeConfig.APPLICATION_NAME
-    val baseAppName = "Daedric.NET X"
+    val baseAppName = "AguiarVieira X"
     logger.warnInBox("Building $baseAppName")
     buildTypes {
         val oidcRedirectSchemeBase = BuildTimeConfig.METADATA_HOST_REVERSED ?: "io.element.android"
